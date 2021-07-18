e97172d8a68bb1840644ec75721197aa122c52ba
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-07-18 14:07:13 +0930

    Update Keyboard_Paranoia.cpp

--- Keyboard_Paranoia.cpp
+++ Keyboard_Paranoia.cpp
@@ -12,43 +12,47 @@ void sound_effects(int track_number)
     {
         play_track = "knocking.wav";
         PlaySound(TEXT("knocking.wav"), NULL, SND_FILENAME | SND_ASYNC);
+        Sleep(5000);
+    }
+    if (track_number == 1)
+    {
+        play_track = "presses_8";
+        PlaySound(TEXT("presses_8"), NULL, SND_FILENAME | SND_ASYNC);
+        Sleep(500);
     }
     std::cout << track_number << ") Playing Track: " << play_track << "\n";
-}
-
-void HideConsole()
-{
-    //::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-    ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
 }
 
 #include <conio.h>
 int main()
 {
-    HideConsole();
+    //::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+    ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
 
     bool flag = false;
     double counter = 0;
     while (1)
     {
+        // TODO: Wait here for next key press.
         //_getch();
         if (GetKeyState('A') & 0x8000 && flag == false)
         {
             std::cout << "Alpha" << "\n";
             sound_effects(0);
             // Allows sound effect to play while pausing program.
-            Sleep(5000);
-            std::cout << "End Sleep" << "\n";
             //flag = true;
         }
         if (GetKeyState('B') & 0x8000 && flag == false)
         {
             std::cout << "Beta" << "\n";
+            sound_effects(1);
             //flag = true;
         }
         if (GetKeyState('Z') & 0x8000 && flag == false)
         {
             std::cout << "ZZZ" << "\n";
+            Sleep(5000);
+            std::cout << "End Sleep" << "\n";
             //flag = true;
         }
     }
